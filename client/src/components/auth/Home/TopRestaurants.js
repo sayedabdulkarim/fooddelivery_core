c6e5ec36e452278bd1fe1f15adc766516ebc3745
unknown <sakarim9124@gmail.com>
2023-11-23 04:24:18 +0530

    updated

--- client/src/components/auth/Home/TopRestaurants.js
+++ client/src/components/auth/Home/TopRestaurants.js
@@ -97,10 +97,12 @@ const TopRestaurantSection = () => {
           <ul ref={carouselViewportRef}>
             {images.map((item, index) => (
               <li key={index}>
-                <img
-                  src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${item.cloudinaryImageId}`}
-                  alt={item.name}
-                />
+                <div className="image_wrapper">
+                  <img
+                    src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${item.cloudinaryImageId}`}
+                    alt={item.name}
+                  />
+                </div>
               </li>
             ))}
           </ul>
