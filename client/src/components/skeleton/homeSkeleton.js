720deea7c7c6bd6bd135ec7267c0f9425ecc3383
unknown <sakarim9124@gmail.com>
2023-11-22 23:49:16 +0530

    updated

--- client/src/components/skeleton/homeSkeleton.js
+++ client/src/components/skeleton/homeSkeleton.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { Skeleton, Row, Col } from "antd";
+import { Skeleton, Row, Col, Layout, List, Avatar } from "antd";
+
+const { Sider, Content } = Layout;
 
 const ShowSkeleton = ({ isLoading, numSkeletons = 40, columnsPerRow = 4 }) => {
-  // Calculate span based on the number of columns per row
   const colSpan = 24 / columnsPerRow;
 
-  // Create an array of Skeletons
   const skeletonGrid = Array.from({ length: numSkeletons }, (_, index) => (
     <Col key={index} span={colSpan}>
       <Skeleton loading={isLoading} active>
@@ -14,7 +14,45 @@ const ShowSkeleton = ({ isLoading, numSkeletons = 40, columnsPerRow = 4 }) => {
     </Col>
   ));
 
-  return <Row gutter={[16, 16]}>{skeletonGrid}</Row>;
+  const listData = Array.from({ length: 3 }).map((_, i) => ({
+    href: "https://ant.design",
+    title: `ant design part ${i + 1}`,
+    avatar: `https://xsgames.co/randomusers/avatar.php?g=pixel&key=${i}`,
+    description:
+      "Ant Design, a design language for background applications, is refined by Ant UED Team.",
+    content:
+      "We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.",
+  }));
+
+  return (
+    <Layout>
+      <Sider width={300} style={{ background: "#fff" }}>
+        <Skeleton loading={isLoading} active avatar>
+          <List
+            itemLayout="horizontal"
+            dataSource={listData}
+            renderItem={(item) => (
+              <List.Item>
+                <List.Item.Meta
+                  avatar={<Avatar src={item.avatar} />}
+                  title={<a href={item.href}>{item.title}</a>}
+                  description={item.description}
+                />
+                {item.content}
+              </List.Item>
+            )}
+          />
+        </Skeleton>
+      </Sider>
+      <Layout style={{ padding: "0 24px 24px" }}>
+        <Content
+          style={{ background: "#fff", padding: 24, margin: 0, minHeight: 280 }}
+        >
+          <Row gutter={[16, 16]}>{skeletonGrid}</Row>
+        </Content>
+      </Layout>
+    </Layout>
+  );
 };
 
 export default ShowSkeleton;
