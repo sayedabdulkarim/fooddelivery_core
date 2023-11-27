ddfbbd33672fb42d78ecdab7de45665974200618
unknown <sakarim9124@gmail.com>
2023-11-27 19:51:34 +0530

    updated

---
+++ client/src/components/auth/Help/GeneralIssue.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const GeneralIssue = () => {
+  return (
+    <div>
+      <h1>GeneralIssue</h1>
+      <h1>GeneralIssue</h1>
+      <h1>GeneralIssue</h1>
+      <h1>GeneralIssue</h1>
+      <h1>GeneralIssue</h1>
+      <h1>GeneralIssue</h1>
+      <h1>GeneralIssue</h1>
+      <h1>GeneralIssue</h1>
+      <h1>GeneralIssue</h1>
+      <h1>GeneralIssue</h1>
+    </div>
+  );
+};
+
+export default GeneralIssue;
--- client/src/screens/auth/HelpScreen.js
+++ client/src/screens/auth/HelpScreen.js
@@ -1,21 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 
-const About = () => {
+import OrderHelp from "../../components/auth/Help/OrderHelp";
+import Faq from "../../components/auth/Help/Faq";
+import Generalissue from "../../components/auth/Help/GeneralIssue";
+import Legal from "../../components/auth/Help/Legal";
+import Super from "../../components/auth/Help/Super";
+
+const HelpScreen = () => {
+  const [activeSection, setActiveSection] = useState("orderHelp");
+
+  let showAccordion;
+  switch (activeSection) {
+    case "orderHelp":
+      showAccordion = <OrderHelp />;
+      break;
+    case "generalissues":
+      showAccordion = <Generalissue />;
+      break;
+    case "legalterms":
+      showAccordion = <Legal />;
+      break;
+    case "faq":
+      showAccordion = <Faq />;
+      break;
+    case "super":
+      showAccordion = <Super />;
+      break;
+    default:
+      showAccordion = <OrderHelp />;
+  }
+
   return (
-    <div>
-      <h1>About</h1>
-      <h1>About</h1>
-      <h1>About</h1>
-      <h1>About</h1>
-      <h1>About</h1>
-      <h1>About</h1>
-      <h1>About</h1>
-      <h1>About</h1>
-      <h1>About</h1>
-      <h1>About</h1>
-      <h1>About</h1>
+    <div className="help_page_container">
+      <div className={"Helptop"}>
+        <div className={"Helptoptext"}>
+          <div>Help & Support</div>
+          <p>Let's take a step ahead and help you better.</p>
+        </div>
+
+        <div className={"Accordianmenu"}>
+          <div className={"Accordian"}>
+            <div className={"AccordianBtn"}>
+              <ul>
+                <li onClick={() => setActiveSection("orderHelp")}>
+                  <NavLink to="/helppage" className={"Nav_link"}>
+                    Help with Orders
+                  </NavLink>
+                </li>
+                <li onClick={() => setActiveSection("generalissues")}>
+                  <NavLink to="/helppage">General Issues</NavLink>
+                </li>
+                <li onClick={() => setActiveSection("legalterms")}>
+                  <NavLink to="/helppage">Legal, Term & Conditions</NavLink>
+                </li>
+                <li onClick={() => setActiveSection("faq")}>
+                  <NavLink to="/helppage">FAQs</NavLink>
+                </li>
+                <li onClick={() => setActiveSection("super")}>
+                  <NavLink to="helppage">Our Super FAQs</NavLink>
+                </li>
+              </ul>
+            </div>
+
+            <div className={"Accordianpage"}>{showAccordion}</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default About;
+export default HelpScreen;
