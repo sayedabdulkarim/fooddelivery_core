95f3e2edacebf7697e0666ef35e84c75b5bfddc8
unknown <sakarim9124@gmail.com>
2023-11-28 18:19:55 +0530

    updated

--- client/src/screens/auth/RestaurantDetails.js
+++ client/src/screens/auth/RestaurantDetails.js
@@ -1,12 +1,9 @@
 import React from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 const RestaurantDetails = () => {
-  const { name, id } = useParams();
-  //     const location = useLocation();
-  //   const queryParams = new URLSearchParams(location.search);
-  //   const id = queryParams.get("id");
-  console.log({ name, id });
+  const { id } = useParams();
+
   return (
     <div>
       <h1>RestaurantDetails: {id}</h1>
