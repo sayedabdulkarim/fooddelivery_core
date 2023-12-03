80211d5b722236df5c62a25d75526b0c961e98b3
unknown <sakarim9124@gmail.com>
2023-12-03 08:52:34 +0530

    updated

--- client/src/components/drawer/CustomDrawer.js
+++ client/src/components/drawer/CustomDrawer.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Drawer } from "antd";
 
-const CustomDrawerComponent = ({ open, onClose, children }) => {
+const CustomDrawerComponent = ({ open, onClose, placement, children }) => {
   return (
     <Drawer
       title=""
-      placement="right"
+      placement={placement}
       onClose={onClose}
       open={open}
       className="custom_drawer"
