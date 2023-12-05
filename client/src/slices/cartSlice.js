2976fe979307932df448ef22bd395127efc28444
unknown <sakarim9124@gmail.com>
2023-12-05 10:54:22 +0530

    updated

--- client/src/slices/cartSlice.js
+++ client/src/slices/cartSlice.js
@@ -1,20 +1,63 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  cart: {},
+  cart: {
+    restaurantDetail: {}, // assuming you set this somewhere else
+    items: [],
+  },
 };
 
 const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
-    addTocart: (state, action) => {},
+    setRestaurantDetail: (state, action) => {
+      state.cart.restaurantDetail = action.payload;
+    },
+    addTocart: (state, action) => {
+      const itemIndex = state.cart.items.findIndex(
+        (item) => item.itemName === action.payload.itemName
+      );
 
-    deleteFromcart: (state, action) => {},
+      if (itemIndex >= 0) {
+        // Item exists, increase the count
+        state.cart.items[itemIndex].count += 1;
+      } else {
+        // Item does not exist, add to the cart with count 1
+        state.cart.items.push({ ...action.payload, count: 1 });
+      }
+    },
+
+    removeFromcart: (state, action) => {
+      const itemIndex = state.cart.items.findIndex(
+        (item) => item.itemName === action.payload.itemName
+      );
+
+      if (itemIndex >= 0) {
+        // If count is more than 1, decrease the count
+        if (state.cart.items[itemIndex].count > 1) {
+          state.cart.items[itemIndex].count -= 1;
+        } else {
+          // If count is 1, remove the item from the cart
+          state.cart.items.splice(itemIndex, 1);
+        }
+      }
+    },
+
+    // You might want to have an action to remove an item regardless of count
+    clearItemFromcart: (state, action) => {
+      state.cart.items = state.cart.items.filter(
+        (item) => item.itemName !== action.payload.itemName
+      );
+    },
   },
 });
 
-export const { setRestaurantDetailsById, setRestaurantDetailsHeaderStick } =
-  cartSlice.actions;
+export const {
+  setRestaurantDetail,
+  addTocart,
+  removeFromcart,
+  clearItemFromcart,
+} = cartSlice.actions;
 
 export default cartSlice.reducer;
