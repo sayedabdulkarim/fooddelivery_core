5e2d193fa40b7ef9f727b24ab1804c0467d7357a
unknown <sakarim9124@gmail.com>
2023-12-07 11:39:30 +0530

    updated

--- server/controller/homeController.js
+++ server/controller/homeController.js
@@ -4,16 +4,20 @@ import BestOffersModal from "../modals/home/bestOffers.js";
 import TopRestaurantModal from "../modals/home/topRestaurants.js";
 import CuisinesModal from "../modals/home/cuisines.js";
 import AllRestaurantsModal from "../modals/home/allRestaurants.js";
+import CartModal from "../modals/cartModal.js";
 
 // @desc Get home page data for offers and restaurants
 // @route GET /api/users/getHomeContent
 // @access PRIVATE
 const getHomePageData = asyncHandler(async (req, res) => {
   try {
+    const userId = req.user._id; // Get the logged-in user's ID
+
     const offerList = await BestOffersModal.find(); // Fetch all documents from BestOffersModal
     const topRestaurantList = await TopRestaurantModal.find(); // Fetch all documents from TopRestaurantModal
     const cuisinesList = await CuisinesModal.find(); // Fetch all documents from CuisinesModal
     const allRestaurantsList = await AllRestaurantsModal.find(); // Fetch all documents from AllRestaurantsModal
+    const userCartDetails = await CartModal.findOne({ userId: userId }); // fetch all cart details from logged in user
 
     res.status(200).json({
       data: {
@@ -21,6 +25,7 @@ const getHomePageData = asyncHandler(async (req, res) => {
         topRestaurantList,
         cuisinesList,
         allRestaurantsList,
+        userCartDetails,
       },
       message: "done successfully",
     });
