c3c27966c0ca636b0a84124e849eaaa3f45e80b5
unknown <sakarim9124@gmail.com>
2023-12-07 15:15:02 +0530

    updated

--- client/src/screens/auth/Orders.js
+++ client/src/screens/auth/Orders.js
@@ -4,7 +4,16 @@ const Orders = () => {
   return (
     <div className="order_component">
       <div className="order_container">
-        <div className="order_container_header"></div>
+        <div className="order_container_header">
+          <div class="order_container_wrapper">
+            <div class="edit_profile">Edit profile</div>
+            <div class="userName"> Abdul</div>
+            <div class="contact">
+              <span className="phone">8296708008</span>
+              <span className="email">sayedtest4747@gmail.com</span>
+            </div>
+          </div>
+        </div>
         {/*  */}
         <div className="order_container_content"></div>
       </div>
