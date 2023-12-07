b3932daf8925227ced8f73d8e025c68fdeb21352
unknown <sakarim9124@gmail.com>
2023-12-07 23:15:08 +0530

    updated

--- client/src/components/auth/Order/OrderContent/OrdersComponent.js
+++ client/src/components/auth/Order/OrderContent/OrdersComponent.js
@@ -16,10 +16,10 @@ const Orders = () => {
     <div class="order_details_component">
       <div class="title"> Past Orders </div>
       {/*  */}
-      <div class="_3xMk0 order_detail_item">
+      <div class="order_detail_item">
         {/*  */}
-        <div class="g28rk item_top">
-          <div class="_359Fc item_top_image">
+        <div class="item_top">
+          <div class="item_top_image">
             <img
               height="200"
               width="300"
@@ -27,23 +27,23 @@ const Orders = () => {
               src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_200,c_fill/cnlhkx4qw7vcrasx8the"
             />
           </div>
-          <div class="_2XWVq item_top_restaurant_details">
-            <div class="_3h4gz title">Chai Point</div>
-            <div class="_2haEe sub_title">Kalyan Nagar</div>
-            <div class="_299_I order_date">
+          <div class="item_top_restaurant_details">
+            <div class="title">Chai Point</div>
+            <div class="sub_title">Kalyan Nagar</div>
+            <div class="order_date">
               ORDER #160896643069945 | Thu, Dec 7, 2023, 11:00 AM
             </div>
-            <div class="_1ziWV view-details">VIEW DETAILS</div>
+            <div class="view-details">VIEW DETAILS</div>
           </div>
         </div>
         {/*  */}
         <div class="_3SKK0 item_bottom">
-          <div class="_33I3_">Banana Cake x 2</div>
-          <div class="_2a27y">
-            <button class="_3PUy8 f4Ovn">REORDER</button>
+          <div class="_33I3_ item_count">Banana Cake x 2</div>
+          <div class="_2a27y reorder">
+            <button class="_3PUy8 f4Ovn isActive">REORDER</button>
             <button class="_3PUy8">HELP</button>
           </div>
-          <div class="_23DHc">
+          <div class="_23DHc payment">
             Total Paid: <span class="_1jGfr"> 479 </span>
           </div>
         </div>
