57075662271a16ee46a1a55116d205b9196b32e2
unknown <sakarim9124@gmail.com>
2023-12-08 20:28:15 +0530

    updated

--- client/src/components/auth/Order/OrderContent/AddressComponent.js
+++ client/src/components/auth/Order/OrderContent/AddressComponent.js
@@ -2,28 +2,28 @@ import React from "react";
 
 const Address = ({ savedAddress }) => {
   return (
-    <div className="_1stFr address_component">
+    <div className=" address_component">
       <div className="address_component_container">
-        <div className="ZMdmv title">Manage Addresses</div>
+        <div className=" title">Manage Addresses</div>
 
-        <div className="_2xL-J address_container_items">
+        <div className=" address_container_items">
           {savedAddress?.addresses?.map((item) => {
             const { _id, address, type } = item;
             return (
               <div
                 key={_id}
-                className="_2-CaT item"
+                className=" item"
                 onClick={() => console.log(item, " iiiiiiiii")}
               >
-                <div className="Pydh7 item_icon">
+                <div className=" item_icon">
                   <span
                     className={`${type === "Home" ? "icon-home" : "icon-work"}`}
                   ></span>
                 </div>
                 <div className="item_desc">
-                  <div className="_3M0On type">{type}</div>
-                  <div className="_1rPE3 address">{address}</div>
-                  <div className="_2o0z3 btn_container">
+                  <div className=" type">{type}</div>
+                  <div className=" address">{address}</div>
+                  <div className=" btn_container">
                     <span>EDIT</span>
                     <span>DELETE</span>
                   </div>
@@ -31,23 +31,6 @@ const Address = ({ savedAddress }) => {
               </div>
             );
           })}
-
-          <div className="_2-CaT" style={{ display: "none" }}>
-            <div className="Pydh7">
-              <span className="icon-work"></span>
-            </div>
-            <div>
-              <div className="_3M0On">Work</div>
-              <div className="_1rPE3">
-                ground floor, Kudlu, Bengaluru, Karnataka 560068, India.
-                (Singasandra)
-              </div>
-              <div className="_2o0z3">
-                <span>EDIT</span>
-                <span>DELETE</span>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
     </div>
