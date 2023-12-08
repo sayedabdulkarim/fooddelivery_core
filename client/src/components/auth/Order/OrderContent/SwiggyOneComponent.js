f8fbe966a25377bd90f15d82185c05be7f7b49a9
unknown <sakarim9124@gmail.com>
2023-12-08 16:07:21 +0530

    updated

--- client/src/components/auth/Order/OrderContent/SwiggyOneComponent.js
+++ client/src/components/auth/Order/OrderContent/SwiggyOneComponent.js
@@ -2,19 +2,51 @@ import React from "react";
 
 const SwiggyOne = () => {
   return (
-    <div>
-      <h1>SwiggyOne</h1>
-      <h1>SwiggyOne</h1>
-      <h1>SwiggyOne</h1>
-      <h1>SwiggyOne</h1>
-      <h1>SwiggyOne</h1>
-      <h1>SwiggyOne</h1>
-      <h1>SwiggyOne</h1>
-      <h1>SwiggyOne</h1>
-      <h1>SwiggyOne</h1>
-      <h1>SwiggyOne</h1>
-      <h1>SwiggyOne</h1>
-      <h1>SwiggyOne</h1>
+    <div className="_1stFr swiggy_one_component">
+      <div className="HaadL swiggy_one_container">
+        {/*  */}
+        <div className="_39L1A swiggy_left_wrapper">
+          <h2 className="_3jx1r text">Swiggy One</h2>
+          <p className="qYzeq desc">
+            Get free delivery and extra discounts all across Swiggy.Your Swiggy
+            One benefits can be availed only on the Swiggy App.
+          </p>
+          <div className="_1lpjo download_section">
+            <a
+              href="https://itunes.apple.com/in/app/id989540920?referrer=utm_source%3Dswiggy%26utm_medium%3Dhomepage"
+              rel="nofollow noopener noreferrer"
+              alt=""
+              target="_blank"
+              className="AppDownloadLinks_appLink__3lQqz"
+              data-testid="ios-appstore"
+            >
+              <img
+                alt=""
+                src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_200,h_65/icon-AppStore_lg30tv"
+              />
+            </a>
+            <a
+              href="https://play.google.com/store/apps/details?id=in.swiggy.android&amp;referrer=utm_source%3Dswiggy%26utm_medium%3Dheader"
+              rel="nofollow noopener noreferrer"
+              alt=""
+              target="_blank"
+              className="AppDownloadLinks_appLink__3lQqz"
+              data-testid="android-playstore"
+            >
+              <img
+                alt=""
+                src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_200,h_65/icon-GooglePlay_1_zixjxl"
+              />
+            </a>
+          </div>
+        </div>
+        {/*  */}
+        <img
+          className="_241Su right_image"
+          alt=""
+          src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_720,h_660/swiggy_one/my_account_super"
+        />
+      </div>
     </div>
   );
 };
