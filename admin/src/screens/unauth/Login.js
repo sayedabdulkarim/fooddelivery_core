7135c8a44c54c916f44edc24ae257f52c1a71824
unknown <sakarim9124@gmail.com>
2023-12-09 16:30:48 +0530

    updated

--- admin/src/screens/unauth/Login.js
+++ admin/src/screens/unauth/Login.js
@@ -1,23 +1,42 @@
 import React from "react";
+import { AuthInfoIcon } from "../../utils/svgs";
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
-      <h1>Login</h1>
-      <h1>Login</h1>
-      <h1>Login</h1>
-      <h1>Login</h1>
-      <h1>Login</h1>
-      <h1>Login</h1>
-      <h1>Login</h1>
-      <h1>Login</h1>
-      <h1>Login</h1>
-      <h1>Login</h1>
-      <h1>Login</h1>
-      <h1>Login</h1>
-      <h1>Login</h1>
-      <h1>Login</h1>
+    <div className="form_container">
+      {/*  */}
+      <div className="title_container">
+        <div className="info_text">
+          Enter a mobile number or restaurant ID to continue
+        </div>
+        <div className="info_icon">
+          <AuthInfoIcon />
+        </div>
+      </div>
+      {/*  */}
+      <form>
+        {/* input */}
+        <div className="css-175oi2r input_item">
+          <input
+            maxLength="10"
+            placeholder="Enter Restaurant ID / Mobile number"
+            autoCapitalize="sentences"
+            autoComplete="on"
+            autoCorrect="on"
+            inputMode="decimal"
+            spellCheck="true"
+            className="css-11aywtz r-6taxm2"
+            value=""
+          />
+        </div>
+        {/* button */}
+        <div className="css-175oi2r r-1awozwy r-1f0042m r-195d4m8 r-1777fci r-1u916t5 r-633pao submit_button">
+          <div className="css-1rynq56 r-1dt6c6u r-1i10wst r-hbpseb r-q4m81j r-ndvcnb r-jwli3a r-lrvibr btn_text">
+            Continue
+          </div>
+        </div>
+        {/*  */}
+      </form>
     </div>
   );
 };
--- admin/src/screens/unauth/index.js
+++ admin/src/screens/unauth/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { AuthInfoIcon, AuthUnderline, ProductIcon } from "../../utils/svgs";
+import LoginComponent from "./Login";
 
 const Index = () => {
   return (
@@ -24,41 +25,7 @@ const Index = () => {
             {/*  */}
             <div className="title_section">Get Started</div>
             {/*  */}
-            <div className="form_container">
-              {/*  */}
-              <div className="title_container">
-                <div className="info_text">
-                  Enter a mobile number or restaurant ID to continue
-                </div>
-                <div className="info_icon">
-                  <AuthInfoIcon />
-                </div>
-              </div>
-              {/*  */}
-              <form>
-                {/* input */}
-                <div className="css-175oi2r input_item">
-                  <input
-                    maxLength="10"
-                    placeholder="Enter Restaurant ID / Mobile number"
-                    autoCapitalize="sentences"
-                    autoComplete="on"
-                    autoCorrect="on"
-                    inputMode="decimal"
-                    spellCheck="true"
-                    className="css-11aywtz r-6taxm2"
-                    value="12"
-                  />
-                </div>
-                {/* button */}
-                <div className="css-175oi2r r-1awozwy r-1f0042m r-195d4m8 r-1777fci r-1u916t5 r-633pao submit_button">
-                  <div className="css-1rynq56 r-1dt6c6u r-1i10wst r-hbpseb r-q4m81j r-ndvcnb r-jwli3a r-lrvibr btn_text">
-                    Continue
-                  </div>
-                </div>
-                {/*  */}
-              </form>
-            </div>
+            <LoginComponent />
           </div>
         </div>
       </div>
