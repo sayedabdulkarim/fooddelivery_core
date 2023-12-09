62aa3b9a1b9a6b41b85712351ef0f4bea372e1db
unknown <sakarim9124@gmail.com>
2023-12-10 00:16:34 +0530

    updated

--- admin/src/components/Header.js
+++ admin/src/components/Header.js
@@ -1,22 +1,36 @@
 import React from "react";
+import { Layout, Menu, Avatar } from "antd";
+import { UserOutlined } from "@ant-design/icons";
 
-const Header = () => {
+const { Header } = Layout;
+
+const AppHeader = () => {
   return (
-    <div>
-      <h1>Header</h1>
-      <h1>Header</h1>
-      <h1>Header</h1>
-      <h1>Header</h1>
-      <h1>Header</h1>
-      <h1>Header</h1>
-      <h1>Header</h1>
-      <h1>Header</h1>
-      <h1>Header</h1>
-      <h1>Header</h1>
-      <h1>Header</h1>
-      <h1>Header</h1>
-    </div>
+    <Layout>
+      <Header
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        {/* Logo */}
+        <div style={{ color: "white", fontSize: "20px" }}>
+          <img
+            src="/path-to-your-logo.png"
+            alt="Logo"
+            style={{ height: "30px" }}
+          />{" "}
+          Logo
+        </div>
+
+        {/* Profile Icon */}
+        <div>
+          <Avatar size="large" icon={<UserOutlined />} />
+        </div>
+      </Header>
+    </Layout>
   );
 };
 
-export default Header;
+export default AppHeader;
