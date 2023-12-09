759bceab6176d37127bf0cf5068b5ac6ff582ac3
unknown <sakarim9124@gmail.com>
2023-12-10 02:02:11 +0530

    updated

--- admin/src/screens/auth/Home.js
+++ admin/src/screens/auth/Home.js
@@ -1,26 +1,37 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const Home = () => {
+  const { userInfo } = useSelector((state) => state.authReducer);
+
   return (
-    <div>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
-      <h1>Home</h1>
+    <div className="home_container">
+      {userInfo?.data?.restaurant ? (
+        <div>
+          <h1 onClick={() => console.log(userInfo?.data?.restaurant)}>Home</h1>
+          <h1>Home</h1>
+          <h1>Home</h1>
+          <h1>Home</h1>
+          <h1>Home</h1>
+          <h1>Home</h1>
+          <h1>Home</h1>
+          <h1>Home</h1>
+          <h1>Home</h1>
+          <h1>Home</h1>
+          <h1>Home</h1>
+          <h1>Home</h1>
+          <h1>Home</h1>
+          <h1>Home</h1>
+          <h1>Home</h1>
+          <h1>Home</h1>
+          <h1>Home</h1>
+          <h1>Home</h1>
+        </div>
+      ) : (
+        <div className="add_restaurant_btn_container">
+          <button>Add Restaurant</button>
+        </div>
+      )}
     </div>
   );
 };
