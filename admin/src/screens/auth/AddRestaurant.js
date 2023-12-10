1825f96e835e77b886d62e574dd0ca7624706cc3
unknown <sakarim9124@gmail.com>
2023-12-10 11:26:59 +0530

    updated

--- admin/src/screens/auth/AddRestaurant.js
+++ admin/src/screens/auth/AddRestaurant.js
@@ -1,15 +1,47 @@
-import React from "react";
-import { Form, Input, Button, Checkbox, Row, Col, Rate } from "antd";
+import React, { useState } from "react";
+import {
+  Form,
+  Input,
+  Button,
+  Checkbox,
+  Row,
+  Col,
+  Rate,
+  DatePicker,
+  TimePicker,
+  Select,
+} from "antd";
 import CuisineTagsInput from "../../utils/FormComponent/TagInput";
+import ImageUploadInput from "../../utils/FormComponent/ImageUploadInput";
 
 const RestaurantForm = ({ onSave }) => {
+  //misc
   const [form] = Form.useForm();
+  const { Option } = Select;
+  const format = "HH:mm"; // 24-hour format
 
+  //start
+  const [imageBase64, setImageBase64] = useState("");
+
+  //func
   const onFinish = (values) => {
-    console.log("Received values from form: ", values);
-    // onSave(values);
+    // Format startTime and nextCloseTime using the 'HH:mm' format
+    const formattedValues = {
+      ...values,
+      startTime: values.startTime
+        ? values.startTime.format("HH:mm")
+        : undefined,
+      nextCloseTime: values.nextCloseTime
+        ? values.nextCloseTime.format("HH:mm")
+        : undefined,
+      imageBase64,
+    };
+
+    console.log("Formatted values for submission:", formattedValues);
+    // onSave(formattedValues);
   };
 
+  console.log(imageBase64, " bbb");
   return (
     <div className="add_restaurant_container">
       <h1 className="title">Add Restaurant</h1>
@@ -52,11 +84,115 @@ const RestaurantForm = ({ onSave }) => {
         </div>
 
         {/*  */}
+        <div className="form_item">
+          <h3 className="section_title">Availability</h3>
+          <Row gutter={16}>
+            <Col span={6}>
+              <Form.Item name="startTime" label="Start Time">
+                <TimePicker format={format} placeholder="Select Start Time" />
+              </Form.Item>
+            </Col>
+            <Col span={6}>
+              <Form.Item name="nextCloseTime" label="Next Close Time">
+                <TimePicker
+                  format={format}
+                  placeholder="Select Next Close Time"
+                />
+              </Form.Item>
+            </Col>
+            <Col span={6}>
+              <Form.Item name="isOpen" valuePropName="checked" label="Open">
+                <Checkbox />
+              </Form.Item>
+            </Col>
+            <Col span={6}>
+              <Form.Item
+                name="isVeg"
+                valuePropName="checked"
+                label="Vegetarian"
+              >
+                <Checkbox />
+              </Form.Item>
+            </Col>
+          </Row>
+        </div>
+
+        {/*  */}
+        <div className="form_item">
+          <h3 className="section_title">Service Level Agreement ( SLA )</h3>
+          <Row gutter={16}>
+            <Col span={6}>
+              <Form.Item
+                name="deliveryTime"
+                label="Delivery Time (mins)"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please enter estimated delivery time",
+                  },
+                ]}
+              >
+                <Input type="number" placeholder="e.g., 30" />
+              </Form.Item>
+            </Col>
+            <Col span={6}>
+              <Form.Item
+                name="lastMileTravel"
+                label="Last Mile Travel (km)"
+                rules={[
+                  {
+                    required: true,
+                    message: "Please enter last mile travel distance",
+                  },
+                ]}
+              >
+                <Input type="number" placeholder="e.g., 5" />
+              </Form.Item>
+            </Col>
+            <Col span={6}>
+              <Form.Item
+                name="serviceability"
+                label="Serviceability"
+                rules={[
+                  { required: true, message: "Please select serviceability" },
+                ]}
+              >
+                <Select placeholder="Select serviceability">
+                  <Option value="SERVICEABLE">Serviceable</Option>
+                  <Option value="NOT_SERVICEABLE">Not Serviceable</Option>
+                </Select>
+              </Form.Item>
+            </Col>
+            <Col span={6}>
+              <Form.Item
+                name="iconType"
+                label="Icon Type"
+                rules={[
+                  { required: true, message: "Please select an icon type" },
+                ]}
+              >
+                <Select placeholder="Select Icon Type">
+                  <Option value="ICON_TYPE_EMPTY">None</Option>
+                  <Option value="ICON_TYPE_STANDARD">Standard</Option>
+                  <Option value="ICON_TYPE_EXPRESS">Express</Option>
+                  {/* Add more icon types as needed */}
+                </Select>
+              </Form.Item>
+            </Col>
+          </Row>
+        </div>
+        {/*  */}
 
         <Row gutter={16}>
           <Col span={8}>
-            <Form.Item name="cloudinaryImageId" label="Cloudinary Image ID">
+            {/* <Form.Item name="cloudinaryImageId" label="Cloudinary Image ID">
               <Input placeholder="Cloudinary Image ID" />
+            </Form.Item> */}
+
+            <Form.Item label="Restaurant Image">
+              <ImageUploadInput
+                onImageUpload={(base64) => setImageBase64(base64)}
+              />
             </Form.Item>
           </Col>
           <Col span={8}>
@@ -64,32 +200,9 @@ const RestaurantForm = ({ onSave }) => {
               <Input placeholder="Cost for Two" />
             </Form.Item>
           </Col>
-          <Col span={8}>
-            <Form.Item name="cuisines" label="Cuisines (comma separated)">
-              <Input placeholder="Cuisines" />
-            </Form.Item>
-          </Col>
         </Row>
 
         {/*  */}
-
-        <Row gutter={16}>
-          <Col span={8}>
-            <Form.Item name="type" label="Type">
-              <Input placeholder="Type" />
-            </Form.Item>
-          </Col>
-          <Col span={8}>
-            <Form.Item name="isOpen" valuePropName="checked" label="Open">
-              <Checkbox />
-            </Form.Item>
-          </Col>
-          <Col span={8}>
-            <Form.Item name="veg" valuePropName="checked" label="Vegetarian">
-              <Checkbox />
-            </Form.Item>
-          </Col>
-        </Row>
 
         <Row gutter={16}>
           <Col span={6}>
---
+++ admin/src/utils/FormComponent/ImageUploadInput.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { Form, Button, Upload, message } from "antd";
+import { PlusOutlined } from "@ant-design/icons";
+
+// Function to get Base64 from file object
+const getBase64 = (file) =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (error) => reject(error);
+  });
+
+const ImageUpload = ({ onImageUpload }) => {
+  const [loading, setLoading] = useState(false);
+
+  const beforeUpload = (file) => {
+    const isImage = file.type.startsWith("image/");
+    if (!isImage) {
+      message.error("You can only upload image files!");
+    }
+    return isImage || Upload.LIST_IGNORE;
+  };
+
+  const handleChange = async (info) => {
+    if (info.file.status === "uploading") {
+      setLoading(true);
+      return;
+    }
+    if (info.file.status === "done") {
+      // Get this url from response in real world.
+      getBase64(info.file.originFileObj).then((base64) => {
+        setLoading(false);
+        onImageUpload(base64); // Pass the Base64 string to the parent component's state.
+      });
+    }
+  };
+
+  const uploadButton = (
+    <div>
+      {loading ? <PlusOutlined /> : <PlusOutlined />}
+      <div style={{ marginTop: 8 }}>Upload</div>
+    </div>
+  );
+
+  return (
+    <Upload
+      name="avatar"
+      listType="picture-card"
+      className="avatar-uploader"
+      showUploadList={false}
+      beforeUpload={beforeUpload}
+      onChange={handleChange}
+    >
+      {uploadButton}
+    </Upload>
+  );
+};
+
+export default ImageUpload;
