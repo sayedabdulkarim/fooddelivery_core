4f81dd1d612e47649664825ad5cf3a8c92e2b669
unknown <sakarim9124@gmail.com>
2023-12-11 22:26:38 +0530

    updated

--- admin/src/screens/auth/AddMenu.js
+++ admin/src/screens/auth/AddMenu.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { Form, Input, InputNumber, Switch, Button } from "antd";
+import { Form, Row, Col, Input, InputNumber, Switch, Button } from "antd";
 import ImageUploadInput from "../../utils/FormComponent/ImageUploadInput";
 
 import { useNavigate } from "react-router-dom";
@@ -29,67 +29,99 @@ const AddMenu = () => {
 
   return (
     <div className="menu_form_container">
-      <h1
+      <h3
         onClick={() =>
           console.log({ categoryName, restaurantMenuDetails }, " categoryName")
         }
+        className="title"
       >
-        AddMenu : {categoryName}
-      </h1>
+        <span>Selected Category</span> : {categoryName}
+      </h3>
       {/*  */}
       <Form form={form} layout="vertical" onFinish={onFinish}>
         {/* Row 1: id, name, and price */}
-        <Form.Item
-          name="id"
-          label="ID"
-          rules={[{ required: true, message: "Please input the item ID" }]}
-        >
-          <Input placeholder="Item ID" />
-        </Form.Item>
-
-        <Form.Item
-          name="name"
-          label="Name"
-          rules={[{ required: true, message: "Please input the item name" }]}
-        >
-          <Input placeholder="Item Name" />
-        </Form.Item>
-
-        <Form.Item
-          name="price"
-          label="Price"
-          rules={[{ required: true, message: "Please input the price" }]}
-        >
-          <InputNumber min={0} placeholder="Price" />
-        </Form.Item>
+        <div className="form_item">
+          <Row gutter={16}>
+            {/* <Col span={6}>
+              <Form.Item
+                name="id"
+                label="ID"
+                rules={[
+                  { required: true, message: "Please input the item ID" },
+                ]}
+              >
+                <Input placeholder="Item ID" />
+              </Form.Item>
+            </Col> */}
+            <Col span={6}>
+              <Form.Item
+                name="name"
+                label="Name"
+                rules={[
+                  { required: true, message: "Please input the item name" },
+                ]}
+              >
+                <Input placeholder="Item Name" />
+              </Form.Item>
+            </Col>
+            <Col span={6}>
+              <Form.Item name="isVeg" label="Veg" valuePropName="checked">
+                <Switch />
+              </Form.Item>
+            </Col>
+            <Col span={6}>
+              <Form.Item
+                name="inStock"
+                label="In Stock"
+                valuePropName="checked"
+              >
+                <Switch />
+              </Form.Item>
+            </Col>
+            <Col span={6}>
+              <Form.Item
+                name="price"
+                label="Price"
+                rules={[{ required: true, message: "Please input the price" }]}
+              >
+                <InputNumber
+                  min={0}
+                  placeholder="Price"
+                  style={{ width: "100%" }}
+                />
+              </Form.Item>
+            </Col>
+          </Row>
+        </div>
 
         {/* Row 2: Description */}
-        <Form.Item
-          name="description"
-          label="Description"
-          rules={[{ required: true, message: "Please input the description" }]}
-        >
-          <Input.TextArea rows={4} placeholder="Description" />
-        </Form.Item>
-
-        {/* Row 3: Image Upload */}
-        <Form.Item
-          label="Restaurant Image"
-          name="imageUpload" // Make sure this matches your form model
-          valuePropName="value"
-          getValueFromEvent={(e) => e}
-          rules={[{ required: true, message: "Please upload an image!" }]}
-        >
-          <ImageUploadInput />
-        </Form.Item>
-
-        {/* In Stock Switch */}
-        <Form.Item name="inStock" label="In Stock" valuePropName="checked">
-          <Switch />
-        </Form.Item>
+        <Row gutter={16}>
+          <Col span={12}>
+            <Form.Item
+              name="description"
+              label="Description"
+              rules={[
+                { required: true, message: "Please input the description" },
+              ]}
+            >
+              <Input.TextArea rows={4} placeholder="Description" />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item
+              label="Restaurant Image"
+              name="imageUpload" // Make sure this matches your form model
+              valuePropName="value"
+              getValueFromEvent={(e) => e}
+              rules={[{ required: true, message: "Please upload an image!" }]}
+            >
+              <ImageUploadInput />
+            </Form.Item>
+          </Col>
+        </Row>
 
         <Form.Item>
-          <Button type="primary" htmlType="submit">
+          <Button type="primary" htmlType="submit" className="submit_btn">
             Add Item
           </Button>
         </Form.Item>
